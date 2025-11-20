7b52156629a50813938f9f43d7a75cf466f383ef
Nicki <96841120+nicoleviarengo@users.noreply.github.com>
2025-11-20 17:42:39 -0300

    urls actualizadas

--- api/urls.py
+++ api/urls.py
@@ -14,15 +14,15 @@ urlpatterns = [
     path('caja/foto/', views.foto_caja_page, name='foto_caja'),
     path('caja/resumen/', views.resumen_caja_page, name='resumen_caja'),
     path('caja/procesar-imagen/', views.procesar_imagen_caja, name='procesar_imagen_caja'),
+    path('caja/confirmar/', views.confirmar_orden_caja, name='confirmar_orden_caja'),
     path('caja/compra-confirmada/', views.compra_confirmada_page, name='compra_confirmada'),
     path('caja/registro-cliente/', views.registro_cliente_page, name='registro_cliente'),
-    path('caja/confirmar/', views.confirmar_orden_caja, name='confirmar_orden_caja'),
     
     # === DEPÓSITO ===
     path('deposito/', views.deposito_page, name='deposito'),
     path('deposito/foto/', views.foto_deposito_page, name='foto_deposito'),
     path('deposito/resumen/', views.resumen_deposito_page, name='resumen_deposito'),
-    path('deposito/procesar-imagen/', views.procesar_imagen_deposito, name='procesar_imagen_deposito'),
-    path('deposito/confirmar/', views.confirmar_inventario_deposito, name='confirmar_inventario_deposito'),
-
+    path('deposito/confirmada/', views.deposito_confirmada_page, name='deposito_confirmada'),
+    path('deposito/historial/', views.historial_deposito_page, name='historial_deposito'),
 ]
+
